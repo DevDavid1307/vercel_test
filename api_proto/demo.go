663150e4762290be5a2b8eb7e972d0ee98b31468
david <devdavid1307@gmail.com>
2023-10-20 14:35:55 +0800

    #

--- api_proto/demo.go
+++ api_proto/demo.go
@@ -13,16 +13,16 @@ type DemoHTTPServer interface {
 func RegisterDemoService(g *gin.Engine, srv DemoHTTPServer) {
 	group := g.Group("/api/demo")
 
-	greeter_HTTP_Handler(group, srv)
+	group.GET("/index", _greeter_HTTP_Handler(srv))
 }
 
-func greeter_HTTP_Handler(g *gin.RouterGroup, srv DemoHTTPServer) {
-	g.GET("/index", func(c *gin.Context) {
+func _greeter_HTTP_Handler(srv DemoHTTPServer) gin.HandlerFunc {
+	return func(c *gin.Context) {
 		resp, err := srv.Greeter("david")
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
 			return
 		}
 		c.JSON(http.StatusOK, gin.H{"message": resp})
-	})
+	}
 }
