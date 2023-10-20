461f50b3b3cbd405b2c27a564bf0afd8099409d0
david <devdavid1307@gmail.com>
2023-10-20 14:21:06 +0800

    #

--- api/demo.go
+++ api/demo.go
@@ -1,17 +1,18 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Demo(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "%s", r.URL.Path)
-	//serve := gin.New()
-	//
-	//serve.GET("/index", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{"message": "Hello from demo endpoint!"})
-	//})
-	//
-	//serve.ServeHTTP(w, r)
+	serve := gin.New()
+	group := serve.Group("/api/demo")
+
+	group.GET("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello from demo endpoint!"})
+	})
+
+	serve.ServeHTTP(w, r)
 }
